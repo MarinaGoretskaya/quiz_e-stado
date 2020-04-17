efb9e3e745a691839739f1d55c694c14919b35b2
unknown <m.goretskaya@caffesta.com>
2020-04-17 11:11:17 +0300

    fix ajax

--- js/script.js
+++ js/script.js
@@ -514,6 +514,8 @@ $(document).ready(function () {
 	let contact;
 	let contactValue;
 
+	let resultData;
+
 	let section1 = body.find('#section-rect-1');
 	let section2 = body.find('#section-rect-2');
 	let section3 = body.find('#section-rect-3');
@@ -522,11 +524,8 @@ $(document).ready(function () {
 	$('#section-rect-1').find('button').click(displaySection1);
 	$('#section-rect-2').find('button').click(displaySection2);
 	$('#section-rect-3').find('button').click(displaySection3);
-	$('#section-rect-4').find('input[type="radio"]').click(displaySection4);
+	$('#section-rect-4').find('input[type="radio"]').click(displayInput);
 	$('#section-rect-4').find('button').click(submitData);
-	// $('#section-rect-4').find('input[type="submit"]').click(submitData);
-
-	// $('#section-rect-1').find('button').click(displaySection);
 
 
 	let allInp = section4.find('.input-txt__wrapper');
@@ -535,10 +534,6 @@ $(document).ready(function () {
 	let inpViber = body.find('#input-viber');
 	let inpTelegram = body.find('#input-telegram');
 	let inpWhatsapp = body.find('#input-whatsapp');
-
-	let formSubmit = body.find('#submit');
-	let ajaxForm = body.find('#ajax_form');
-	let resultForm = body.find('#result_form');
 
 	body.find('#inp-phone, #inp-viber, #inp-telegram, #inp-whatsapp').mask("+375 (99) 999-99-99");
 	body.find('#inp-email').inputmask("email");
@@ -570,7 +565,7 @@ $(document).ready(function () {
 		position = section3.find('input:checked').val();
 	}
 
-	function displaySection4() {
+	function displayInput() {
 		contact = section4.find('input:checked').val();
 
 		if (!contact) {
@@ -610,17 +605,20 @@ $(document).ready(function () {
 			contact = 'Телефон';
 		}
 		checkContactValue(contact);
+
 		if (!contactValue || (contactValue === 'Вы не ввели данные')) {
 			contactValue = 'Вы не ввели данные';
 			alert(contactValue);
 			return;
 		}
-		section4.html('div').html('Количество дойных коров: ' + countCow + '. Вы доите ' + countMilk + ' л молока в сутки. Вы – ' + position + '. Ваш ' + contact + ': ' + contactValue + '.');
+
+		resultData = 'Количество дойных коров: ' + countCow + '. Вы доите ' + countMilk + ' л молока в сутки. Вы – ' + position + '. Ваш ' + contact + ': ' + contactValue + '.';
+		section4.html('div').html(resultData);
 
 		allInp.addClass('display-none');
 		inpTel.removeClass('display-none');
 
-		sendForm('result_form', 'ajax_form', 'send.php');
+		sendForm('ajax_form', 'send.php', resultData);
 
 		open("https://www.figma.com/file/NgF3FzxtgvbiZqmprLURlS/e-stado-%D0%B4%D0%B8%D0%B7%D0%B0%D0%B9%D0%BD?node-id=0%3A1");
 	}
@@ -648,21 +646,21 @@ $(document).ready(function () {
 		}
 	}
 
-	function sendForm(result_form, ajax_form, url) {
+	function sendForm(ajax_form, url, resultData) {
+		// let result;
 		$.ajax({
 			url: url,
 			type: 'POST',
 			dataType: "html",
-			data: $("#" + ajax_form).serialize(),
+			data: $("#" + ajax_form).serialize() + resultData,
 			success: function (response) {
-				result = $.parseJSON(response);
-				$('#result_form').html('e-mail: ' + result.email);
+				// result = $.parseJSON(response);
+				console.log('Данные отправлены.');
 			},
 			error: function () {
 				console.log('Ошибка. Данные не отправлены.');
 			}
 		});
 	}
-
 });
 
--- send.php
+++ send.php
@@ -2,19 +2,19 @@
 
 if(isset($_POST['submit'])){
     $email = $_POST['email'];
-    $to = $email; // Здесь нужно написать e-mail, куда будут приходить письма
-//    $from = "no-reply(собака)epicblog.net"; // Здесь нужно написать e-mail, от кого будут приходить письма, например no-reply(собака)epicblog.net
+    $resultData = $_POST['resultData'];
+
+    $to = $email;
+    //    $from = "https://leadme.agency";
     if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $email))
     {
         show_error("<br /> Е-mail адрес не существует");
     }
 
-    /* Переменная, которая будет отправлена на почту со значениями, вводимых в поля */
     $mail_to_myemail = "Здравствуйте! 
-Было отправлено сообщение с сайта! 
-E-mail: $email
-
-Чтобы ответить на письмо, создайте новое сообщение, скопируйте электронный адрес и вставьте в поле Кому.";
+Было отправлено сообщение с сайта $from!
+Ваш E-mail: $email.
+resultData.";
 
     $headers = "From: $from \r\n";
 
@@ -26,6 +26,6 @@ E-mail: $email
 ?>
 <!--Переадресация на главную страницу сайта, через 3 секунды-->
 <script language="JavaScript" type="text/javascript">
-	function changeurl(){eval(self.location="https://epicblog.net");}
+	function changeurl(){eval(self.location="https://leadme.agency");}
 	window.setTimeout("changeurl();",3000);
 </script>
